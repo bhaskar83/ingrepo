69fb311ba673095e6e35c03a18c26ed9e3e672f9
javacloudmc439 <javacloudmc439@ctsjcloud462.iiht.tech>
2020-10-27 17:32:36 +0530

    Urekadiscovery

---
+++ LoanServer/src/main/java/com/ing/LoanMicroserviceEurekaDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LoanMicroserviceEurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMicroserviceEurekaDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ LoanServer/src/test/java/com/ing/LoanMicroserviceEurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanMicroserviceEurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
